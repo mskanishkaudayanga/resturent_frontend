97c4872294cbaeda62f5cdc0c0657b8d1b4544e7
kanishka udayanga <119073238+mskanishkaudayanga@users.noreply.github.com>
2024-10-27 15:57:05 +0530

    comm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+        },
+      },
+    },
+  },
+});
